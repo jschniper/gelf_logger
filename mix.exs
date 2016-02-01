f4d8d525b2c66919c74ce75f8bf2dbe9fec9d2e0
Joshua Schniper <Joshua.Schniper@chiprewards.com>
2016-02-01 17:16:00 -0600

    Bump version and publish

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule GelfLogger.Mixfile do
 
   def project do
     [app: :gelf_logger,
-     version: "0.0.1",
+     version: "0.1.0",
      elixir: "~> 1.1",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
