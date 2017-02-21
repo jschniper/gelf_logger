51f161189ffc6c21459cff5a941e1b27eb7c184e
Philipp Neugebauer <ppn@philippneugebauer.net>
2017-02-21 17:54:09 +0100

    fixing the elixir warnings

--- mix.exs
+++ mix.exs
@@ -7,9 +7,9 @@ defmodule GelfLogger.Mixfile do
      elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     deps: deps,
-     description: description,
-     package: package,
+     deps: deps(),
+     description: description(),
+     package: package(),
 
      # Docs
      name: "GELF Logger",
@@ -34,10 +34,10 @@ defmodule GelfLogger.Mixfile do
      {:ex_doc, "~> 0.14", only: :dev}
    ]
   end
-  
+
   defp description do
     """
-      A Logger backend that will generate Graylog Extended Log Format messages and 
+      A Logger backend that will generate Graylog Extended Log Format messages and
       send them to a compatible server.
     """
   end
