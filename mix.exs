5d5d0e1d37199c5ae8d919cd7cc834d3ed0c11ff
Joshua Schniper <Joshua.Schniper@chiprewards.com>
2018-05-17 09:35:39 -0500

    Update version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule GelfLogger.Mixfile do
 
   def project do
     [app: :gelf_logger,
-     version: "0.7.4",
+     version: "0.7.5",
      elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
